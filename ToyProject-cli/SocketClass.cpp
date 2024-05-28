1f968dd2a652acb4493c53587acedf75f4473404
jang-taehyun <jth0202@naver.com>
2024-05-28 16:36:14 +0900

    finish

--- ToyProject-cli/SocketClass.cpp
+++ ToyProject-cli/SocketClass.cpp
@@ -13,11 +13,6 @@ bool SocketClass::init()
 	int FunctionResult;
 	struct hostent* host;
 
-	host = gethostbyname("serv-alb-928864148.ap-northeast-2.elb.amazonaws.com");
-	if (!host)
-		return false;
-
-
 	FunctionResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (FunctionResult)
 	{
@@ -39,7 +34,7 @@ bool SocketClass::init()
 
 	memset(&ServerAddress, 0, sizeof(ServerAddress));
 	ServerAddress.sin_family = AF_INET;
-	ServerAddress.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*)host->h_addr_list[0]));
+	ServerAddress.sin_addr.s_addr = inet_addr("13.124.5.97");
 	ServerAddress.sin_port = htons(3828);
 
 	FunctionResult = connect(ConnectSocket, (SOCKADDR*)&ServerAddress, sizeof(ServerAddress));
